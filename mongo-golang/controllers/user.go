c67084f943a08271b5425dd20faf09fb24a7a396
Baalekshan <69910615+Baalekshan@users.noreply.github.com>
2023-11-18 18:36:28 +0530

    Update user.go

--- mongo-golang/controllers/user.go
+++ mongo-golang/controllers/user.go
@@ -1,12 +1,14 @@
 package controllers
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
+	"mongo-golang/models"
+	"net/http"
+
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
-	"net/http"
 )
 
 type UserController struct {
@@ -17,17 +19,18 @@ func NewUserController(s *mgo.Session) *UserController {
 	return &UserController{s}
 }
 
-func (uc UserController) GetUser (w http.ResponseWriter, r *http.Request) {
-	id  := p.ByName("id")
+func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
 
 	if !bson.IsObjectIdHex(id) {
 		w.WriteHeader(http.StatusNotFound)
+		return
 	}
 
-	iod := bson.ObjectIdHex(id)
+	oid := bson.ObjectIdHex(id)
 
-	u := models.user{}
-	if err := uc.Session.DB("mongo.golang").C("users").FindId(oid) {
+	u := models.User{}
+	if err := uc.Session.DB("mongo-golang").C("users").FindId(oid); err != nil {
 		w.WriteHeader(404)
 		return
 	}
@@ -41,5 +44,38 @@ func (uc UserController) GetUser (w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "%s\n", uj)
 }
-CreateUser
-DeleteUser
+
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request) {
+	u := models.User{}
+	json.NewDecoder(r.Body).Decode(&u)
+	u.Id = bson.NewObjectId()
+	uc.Session.DB("mongo-golang").C("users").Insert(u)
+	uj, err := json.Marshal(u)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "%s\n", uj)
+
+}
+
+func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(404)
+		return
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	if err := uc.Session.DB("mongo-golang").C("users").RemoveId(oid); err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Deleted User ", oid, "\n")
+}
